6871e46ce8a84d36f19a51e163437ddd10731d1f
Vaishak V K <vaishakvk@Vaishaks-MacBook-Air.local>
2021-08-10 16:11:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import * as React from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+// let token = "";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [token, setToken] = React.useState("");
+    const firebaseConfig = {
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: "",
+    };
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    } else {
+        firebase.app(); // if already initialized, use that one
+    }
+    // firebase.initializeApp(firebaseConfig);
+
+    const auth = firebase.auth();
+    const email = "tester_vkvkv@yopmail.com";
+
+    React.useEffect(async () => {
+        if (auth.isSignInWithEmailLink(window.location.href)) {
+            const result = await auth.signInWithEmailLink(
+                email,
+                window.location.href
+            );
+            const jwt = await result.user.getIdToken();
+            console.log(String(jwt));
+            setToken(jwt);
+        }
+    }, []);
+
+    const login = async () => {
+        auth.sendSignInLinkToEmail(email, {
+            url: "http://localhost:3000",
+            handleCodeInApp: true,
+        }).catch((err) => {
+            console.log(err);
+        });
+    };
+    return (
+        <div className="App">
+            {token ? (
+                <p>Your token {token} </p>
+            ) : (
+                <button onClick={login}>LOGIN</button>
+            )}
+        </div>
+    );
 }
 
 export default App;
